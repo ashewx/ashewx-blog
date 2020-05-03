788f325e54642b0a88c35231456a85c98929ac02
Andrew <ashewx@gmail.com>
2020-05-02 23:17:21 -0700

    Fix links

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { withStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
-import SvgIcon from '@material-ui/core/SvgIcon';
-import AppBarWrapper from './AppBarWrapper';
-import Button from '../Button';
-import Toolbar, { styles as toolbarStyles } from '../Toolbar';
-import { ReactComponent as Logo } from '../../assets/ANoutline.svg'
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { withStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import AppBarWrapper from "./AppBarWrapper";
+import Button from "../Button";
+import Toolbar, { styles as toolbarStyles } from "../Toolbar";
+import { ReactComponent as Logo } from "../../assets/ANoutline.svg";
 
 const styles = (theme) => ({
   title: {
-    display: 'flex'
+    display: "flex",
   },
   placeholder: toolbarStyles(theme).root,
   toolbar: {
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   left: {
     flex: 1,
   },
   right: {
     flex: 1,
-    display: 'flex',
-    justifyContent: 'flex-end',
+    display: "flex",
+    justifyContent: "flex-end",
   },
   rightLink: {
-    fontSize: 16,
-    color: theme.palette.common.white
+    fontSize: 20,
+    color: theme.palette.common.white,
   },
   logo: {
     width: theme.spacing(8),
     height: theme.spacing(8),
-  }
+  },
 });
 
 class AppBar extends React.Component {
@@ -54,37 +54,27 @@ class AppBar extends React.Component {
               </SvgIcon>
             </Link>
             <div className={classes.right}>
-              <Button>
-                <Link
-                  variant="h6"
-                  underline="none"
-                  className={clsx(classes.rightLink)}
-                  href="/blog/"
-                >
-                  Blog
-                </Link>
-              </Button>
-              <Button>
-                <Link
-                  color="inherit"
-                  variant="h6"
-                  underline="none"
-                  className={classes.rightLink}
-                  href="/photography/"
-                >
-                  Photography
-                </Link>
-              </Button>
-              <Button>
-                <Link
-                  variant="h6"
-                  underline="none"
-                  className={clsx(classes.rightLink)}
-                  href="/videos/"
-                >
-                  Videos
-                </Link>
-              </Button>
+              <Link
+                underline="none"
+                className={clsx(classes.rightLink)}
+                href="/blog"
+              >
+                <Button>Blog</Button>
+              </Link>
+              <Link
+                underline="none"
+                className={clsx(classes.rightLink)}
+                href="/photography"
+              >
+                <Button>Photography</Button>
+              </Link>
+              <Link
+                underline="none"
+                className={clsx(classes.rightLink)}
+                href="/videos"
+              >
+                <Button>Videos</Button>
+              </Link>
             </div>
           </Toolbar>
         </AppBarWrapper>
