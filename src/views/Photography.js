da86b6e0cc8b3ccae66ad0a26cda84ad04544b59
Andrew <ashewx@gmail.com>
2021-04-04 20:56:04 -0700

    Show image on click

--- src/views/Photography.js
+++ src/views/Photography.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import ButtonBase from "@material-ui/core/ButtonBase";
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import axios from "axios";
 
 const styles = (theme) => ({
@@ -49,6 +51,20 @@ const styles = (theme) => ({
     opacity: 0,
     transition: theme.transitions.create("opacity"),
   },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+  innerBackdrop: {
+    height: 'calc(100% - 70px)',
+    width: '100%',
+    top: '70px',
+    position: 'fixed',
+    textAlign: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    alignItems: 'center',
+  }
 });
 
 class Photography extends React.Component {
@@ -59,6 +75,7 @@ class Photography extends React.Component {
       screenWidth: window.innerWidth,
       screenHeight: window.innerHeight,
       activePhoto: null,
+      open: false,
     };
   }
 
@@ -81,6 +98,7 @@ class Photography extends React.Component {
               ...this.state.images,
               {
                 url: `${x}=w500`,
+                urlBig: `${x}=w1920`,
                 urlPre: x,
                 height,
                 width,
@@ -104,6 +122,20 @@ class Photography extends React.Component {
     });
   }
 
+  handleClose = () => {
+    this.setState({
+      open: false,
+      activePhoto: null
+    });
+  };
+
+  selectPhoto = (index) => {
+    this.setState({
+      open: true,
+      activePhoto: index
+    });
+  };
+
   render() {
     const { classes } = this.props;
     const images = this.state.images;
@@ -112,7 +144,7 @@ class Photography extends React.Component {
     return (
       <Container className={classes.root}>
         <section className={classes.section}>
-          {images.map((x) => (
+          {images.map((x, i) => (
             <ButtonBase
               focusRipple
               className={classes.imgCont}
@@ -120,7 +152,9 @@ class Photography extends React.Component {
                 width: `${(x.width * imgRatio) / x.height}px`,
                 flexGrow: `${(x.width * imgRatio) / x.height}px`,
               }}
+              onClick={() => this.selectPhoto(i)}
               focusVisibleClassName={classes.focusVisible}
+              key={i}
             >
               <i
                 className={classes.separate}
@@ -131,6 +165,14 @@ class Photography extends React.Component {
             </ButtonBase>
           ))}
         </section>
+        <Backdrop className={classes.backdrop} open={this.state.open} onClick={this.handleClose}>
+          <div className={classes.innerBackdrop}>
+            {this.state.activePhoto ?
+            <img style={{height:'100%'}} src={this.state.images[this.state.activePhoto].urlBig} alt="" loading="lazy" />:
+            <CircularProgress color="secondary" size={250} />
+            }
+          </div>
+        </Backdrop>
       </Container>
     );
   }
